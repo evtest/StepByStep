c660b6bf4774126dd2b93437f6cd33202c31d839
evtest <mevmix@gmail.com>
2014-08-15 13:07:56 +0300

    Added

--- ClassLibrary/ClassLibrary/Class1.cs
+++ ClassLibrary/ClassLibrary/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Threading;
 
 namespace ClassLibrary
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void AssertClass()
+        {
+            Assert.IsTrue(true);
+            Thread.Sleep(5000);
+        }
     }
 }
